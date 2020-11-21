2aaf5738beb1aae9bb71cd2e770452e08e0b3436
ipospelov <ivan.pospelov@stepik.org>
2020-11-21 11:06:03 +0700

    Fix lab 2.2 (integration using infinite sequence)

--- src/lab2/lab2_2.clj
+++ src/lab2/lab2_2.clj
@@ -7,17 +7,23 @@
   (* (/ (+ (fun x) (fun (+ x step-size))) 2) step-size))
 
 (defn integral-sequence
-  [fun x step-size]
+  [fun step-size]
   (reductions (fn [res i] (+ res (delta fun i step-size)))
-              (range 0 (+ x step-size) step-size)))
+              (map #(* step-size %) (range))))
 
-(defn integrate
-  [fun x step-size]
-  (nth (integral-sequence fun x step-size) (- (/ x step-size) 1)))
+(defn get-integrator
+  [fun step-size]
+  (fn [x] (nth (integral-sequence fun step-size) (- (/ x step-size) 1))))
+
+(let [integrate-f (get-integrator (fn [x] x) 0.01)]
+  (println (time (integrate-f 1000)))
+  (println (time (integrate-f 1010)))
+  (println (time (integrate-f 2000))))
 
 (test/deftest test
   (test/testing "Testing lab2_2"
-    (test/is (= (integrate (fn [x] x) 10 0.1) 99/2))
-    (test/is (= (integrate (fn [x] (* x x)) 4 1) 43/2))))
+    (let [integrate-f (get-integrator (fn [x] x) 1)]
+      (test/is (= (integrate-f 10) 99/2))
+      (test/is (= (integrate-f 100) 9999/2)))))
 
 (test/run-tests 'lab2.lab2-2)
